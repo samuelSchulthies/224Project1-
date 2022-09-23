d4c36aaf93406b0d1f5b21609c08eab1a121660c
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-22 22:49:47 -0600

    Add initial files

--- myxxd/myxxd.c
+++ myxxd/myxxd.c
@@ -45,16 +45,16 @@ void printDataAsHex(unsigned char *data, size_t size) {
 
     printf(" ");
     for (int i = 0; i < size; i++){
-//        if (i == size - 1){
-//            printf("0");
-//        }
+        if (data[i] == '\n'){
+            printf("0a");
+            break;
+        }
         printf("%x", data[i]);
         counter++;
         if (counter % 2 == 0){
             printf(" ");
         }
     }
-    //printf("0a");
 }
 
 /**
