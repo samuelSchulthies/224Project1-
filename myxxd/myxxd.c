ea3acd91441f2b40712a270df05aea1ccbe44287
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-22 21:55:38 -0600

    Add initial files

--- myxxd/myxxd.c
+++ myxxd/myxxd.c
@@ -41,10 +41,9 @@ FILE *parseCommandLine(int argc, char **argv, int *bits) {
  * size: the size of the array
  **/
 void printDataAsHex(unsigned char *data, size_t size) {
-    //for (int i = 0; i > size - 1; i++){
-        //printf("%x \n YYEEE HAW", data[i]);
-    //}
-    printf("YEEEHAW");
+    for (int i = 0; i > size; i++){
+        printf("%x ", data[i]);
+    }
 }
 
 /**
