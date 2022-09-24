2f83fdd4aa195522d1333d76caa2355029668889
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-24 14:21:13 -0600

    Add initial files

--- myxxd/myxxd.c
+++ myxxd/myxxd.c
@@ -20,18 +20,18 @@ int BYTESPACES;
  * returns the input file pointer (FILE*)
  **/
 FILE *parseCommandLine(int argc, char **argv, int *bits) {
-  if (argc > 2) {
-    printf("Usage: %s [-b|-bits]\n", argv[0]);
-    exit(BAD_NUMBER_ARGS);
-  }
+    if (argc > 2) {
+        printf("Usage: %s [-b|-bits]\n", argv[0]);
+        exit(BAD_NUMBER_ARGS);
+    }
 
-  if (argc == 2 && (strcmp(argv[1], "-b") == 0 || strcmp(argv[1], "-bits") == 0)) {
-    *bits = TRUE;
-  } else {
-    *bits = FALSE;
-  }
+    if (argc == 2 && (strcmp(argv[1], "-b") == 0 || strcmp(argv[1], "-bits") == 0)) {
+        *bits = TRUE;
+    } else {
+        *bits = FALSE;
+    }
 
-  return stdin;
+    return stdin;
 }
 
 /**
@@ -46,10 +46,10 @@ void printDataAsHex(unsigned char *data, size_t size) {
     int counter = 0;
     BYTESPACES = 0;
     printf("  ");
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         printf("%02x", data[i]);
         counter++;
-        if ((counter % 2 == 0) && (i < size - 1)){
+        if ((counter % 2 == 0) && (i < size - 1)) {
             printf(" ");
             BYTESPACES++;
         }
@@ -66,35 +66,52 @@ void printDataAsHex(unsigned char *data, size_t size) {
  * size: the size of the array
  **/
 void printDataAsChars(unsigned char *data, size_t size) {
-    //printf("\nsize: %lx\n", size);
     int spacer = 39 - (size * 2) - BYTESPACES;
-//    printf("spacer: %d\n", spacer);
-//    printf("BYTSPACES: %d\n", BYTESPACES);
 
-    for (int i = 0; i < spacer; i++){
-        if (size == 16){
+    for (int i = 0; i < spacer; i++) {
+        if (size == 16) {
             break;
         }
         printf(" ");
     }
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         printf("%c", data[i]);
     }
 }
 
-void readAndPrintInputAsHex(FILE *input) {
-  unsigned char data[16];
-  int numBytesRead = fread(data, 1, 16, input);
-  unsigned int offset = 0;
-  while (numBytesRead != 0) {
-    printf("%08x:", offset);
-    offset += numBytesRead;
-    printDataAsHex(data, numBytesRead);
+void printDataAsBits(unsigned char *data, size_t size){
+    int counter = 0;
+    BYTESPACES = 0;
     printf("  ");
-    printDataAsChars(data, numBytesRead);
-    printf("\n");
-    numBytesRead = fread(data, 1, 16, input);
-  }
+    for (int i = 0; i < size; i++) {
+        if (data[i] % 2 == 1){
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+        counter++;
+        if ((counter % 2 == 0) && (i < size - 1)) {
+            printf(" ");
+            BYTESPACES++;
+        }
+
+    }
+}
+
+void readAndPrintInputAsHex(FILE *input) {
+    unsigned char data[16];
+    int numBytesRead = fread(data, 1, 16, input);
+    unsigned int offset = 0;
+    while (numBytesRead != 0) {
+        printf("%08x:", offset);
+        offset += numBytesRead;
+        printDataAsHex(data, numBytesRead);
+        printf("  ");
+        printDataAsChars(data, numBytesRead);
+        printf("\n");
+        numBytesRead = fread(data, 1, 16, input);
+    }
 }
 
 /**
@@ -105,17 +122,28 @@ void readAndPrintInputAsHex(FILE *input) {
  * input: input stream
  **/
 void readAndPrintInputAsBits(FILE *input) {
-  printf("TODO 3: readAndPrintInputAsBits\n");
+    unsigned char data[16];
+    int numBytesRead = fread(data, 1, 16, input);
+    unsigned int offset = 0;
+    while (numBytesRead != 0) {
+        printf("%08x:", offset);
+        offset += numBytesRead;
+        printDataAsBits(data, numBytesRead);
+        printf("  ");
+        printDataAsChars(data, numBytesRead);
+        printf("\n");
+        numBytesRead = fread(data, 1, 16, input);
+    }
 }
 
 int main(int argc, char **argv) {
-  int bits = FALSE;
-  FILE *input = parseCommandLine(argc, argv, &bits);
+    int bits = FALSE;
+    FILE *input = parseCommandLine(argc, argv, &bits);
 
-  if (bits == FALSE) {
-    readAndPrintInputAsHex(input);
-  } else {
-    readAndPrintInputAsBits(input);
-  }
-  return 0;
+    if (bits == FALSE) {
+        readAndPrintInputAsHex(input);
+    } else {
+        readAndPrintInputAsBits(input);
+    }
+    return 0;
 }
