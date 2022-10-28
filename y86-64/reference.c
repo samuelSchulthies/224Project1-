e609deabddf1fc3982666fb16677b515b2320884
samuelSchulthies <samuelschulthies@gmail.com>
2022-10-28 15:09:53 -0600

    Add initial files

--- y86-64/reference.c
+++ y86-64/reference.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 void setValueTo200(int *a){
+    //WRITE CODE TO SET THE VALUE OF a TO 200
+    //THIS SHOULD CHANGE THE VARIABLE IN MAIN
     *a = 200;
 }
 
 void add24AndPrint(int a){
+    //WRITE CODE TO ADD 24 TO a AND THEN PRINT OUT
     *(&a) += 24;
     printf("The value of a is: %d\n", a);
 }
